0105185cccfbebe128851dac619416e8b0189e08
sirasistant <sirasistant@gmail.com>
2017-08-19 17:55:02 +0200

    Implemented a failure test

--- test/remittance.js
+++ test/remittance.js
@@ -1,5 +1,32 @@
 var Remittance = artifacts.require("./Remittance.sol");
 
+var expectedExceptionPromise = function (action, gasToUse) {
+  return new Promise(function (resolve, reject) {
+    try {
+      resolve(action());
+    } catch (e) {
+      reject(e);
+    }
+  })
+    .then(function (txn) {
+      // https://gist.github.com/xavierlepretre/88682e871f4ad07be4534ae560692ee6
+      return web3.eth.getTransactionReceiptMined(txn);
+    })
+    .then(function (receipt) {
+      // We are in Geth
+      assert.equal(receipt.gasUsed, gasToUse, "should have used all the gas");
+    })
+    .catch(function (e) {
+      if ((e + "").indexOf("invalid JUMP") || (e + "").indexOf("out of gas") > -1) {
+        // We are in TestRPC
+      } else if ((e + "").indexOf("please check your gas amount") > -1) {
+        // We are in Geth for a deployment
+      } else {
+        throw e;
+      }
+    });
+};
+
 contract('Remittance', function (accounts) {
   var instance;
   var pass1 = "hola";
@@ -25,7 +52,7 @@ contract('Remittance', function (accounts) {
   })
 
   it("should register a challenge", () => {
-    return instance.registerChallenge(doubleHash1, doubleHash2, 10000, { from: accounts[0],value: 120000000000000 })
+    return instance.registerChallenge(doubleHash1, doubleHash2, 10000, { from: accounts[0], value: 120000000000000 })
       .then(() => {
         return instance.challenges(accounts[0], { from: accounts[0] });
       }).then((challenge) => {
@@ -36,9 +63,18 @@ contract('Remittance', function (accounts) {
 
   it("should be able to solve a challenge", () => {
 
-    return instance.registerChallenge(doubleHash1, doubleHash2, 10000, { from: accounts[1],value: 120000000000000 })
+    return instance.registerChallenge(doubleHash1, doubleHash2, 10000, { from: accounts[1], value: 120000000000000 })
       .then(() => {
         return instance.solveChallenge(hash1, hash2, accounts[1], { from: accounts[2] })
+      });
+  });
+
+  it("shouldnt be able to create a new challenge with other one opened", () => {
+    return instance.registerChallenge(doubleHash1, doubleHash2, 10000, { from: accounts[1], value: 120000000000000 })
+      .then(() => {
+        return expectedExceptionPromise(function () {
+          return MyContract.new(doubleHash1, doubleHash2, 10000, { from: accounts[1], value: 120000000000000, gas: 3000000 });
+        }, 3000000);
       });
   });
 
